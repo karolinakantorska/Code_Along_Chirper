f593ea77a3a172ed2823c5a1cde400fd1ae73b39
Your Name <karolina.kantorska@gmail.com>
2019-12-09 14:43:28 +0100

    Dashboard Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData} from '../actions/shared'
+import Dashboard from './Dashboard'
 
 
 class App extends Component {
@@ -10,10 +11,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        // if this.props.loading is true we render null
+        {this.props.loading === true ? null
+        // if this.props.loading is false we render Dashboard component
+        : <Dashboard/> }
       </div>
     )
   }
 }
 
-export default connect()(App)
+// to render a Daschboard component only when loading initial data is finished
+function mapStateToProps({ authedUser }) {
+  return {
+    // when authedUser equals null then loading is true
+    loading: authedUser === null
+  }
+}
+export default connect(mapStateToProps)(App)
